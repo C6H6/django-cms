e04f5f29d156ed47c3cbf84071253a42883bcd6d
C6H6 <c3c3h3h3@gmail.com>
2018-11-03 18:38:10 +0100

    Add template

--- cms/apps/contact/views.py
+++ cms/apps/contact/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Contact page")
+    return render(request, 'contact/index.html')
