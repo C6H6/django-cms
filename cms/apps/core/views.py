b050c6abe9697dd4160796202dc729ab8daf3ac2
C6H6 <c3c3h3h3@gmail.com>
2018-12-22 16:35:35 +0100

    Promotion on home page

--- cms/apps/core/views.py
+++ cms/apps/core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from cms.apps.travel.models import Travel
+
 
 def index(request):
-    return render(request, 'core/index.html')
+    promoted = Travel.objects.filter(promotion_on_homepage=True, active=True)
+    return render(request, 'core/index.html', {'promoted': promoted})
--- cms/apps/travel/models.py
+++ cms/apps/travel/models.py
@@ -6,6 +6,7 @@ class Travel(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     active = models.BooleanField(default=True)
+    promotion_on_homepage = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
