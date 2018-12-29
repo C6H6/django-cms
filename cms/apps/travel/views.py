cc55c1a02e2c1f4be7127190ad29f7b9333ed44d
C6H6 <c3c3h3h3@gmail.com>
2018-12-29 20:16:45 +0100

    Travels searching

--- cms/apps/travel/views.py
+++ cms/apps/travel/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render, get_object_or_404
 
 from cms.apps.travel.models import Travel
+from django.db.models import Max, Min
 
 
 def index(request):
-    travels = Travel.objects.filter(active=True)
-    return render(request, 'travel/index.html', {'travels': travels})
+    get = request.GET
+    params = get_params(get)
+    price_range = {'max': Travel.objects.all().aggregate(Max('price')),
+                   'min': Travel.objects.all().aggregate(Min('price'))}
+    travels = Travel.objects.filter(**params)
+    return render(request, 'travel/index.html', {'travels': travels, 'params': params, 'price_range': price_range})
 
 
 def details(request, travel_id):
     travel = get_object_or_404(Travel, pk=travel_id, active=True)
     return render(request, 'travel/details.html', {'travel': travel})
+
+
+def get_params(params):
+    result = {'active': True}
+    if params.get('name'):
+        result['title__contains'] = params.get('name')
+    if params.get('country'):
+        result['country'] = params.get('country')
+    if params.get('price'):
+        result['price__range'] = params.getlist('price')
+
+    return result
