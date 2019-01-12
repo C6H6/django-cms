5f8dd7a8d13e06cf1957916b9132799ab1c97101
C6H6 <c3c3h3h3@gmail.com>
2019-01-12 17:07:32 +0100

    Contact template

--- cms/apps/contact/forms.py
+++ cms/apps/contact/forms.py
@@ -1,8 +1,19 @@
+from django import forms
 from django.forms import ModelForm
 from cms.apps.contact.models import Inquiry
+from django.forms.widgets import TextInput
 
 
 class InquiryForm(ModelForm):
+    name = forms.CharField(
+        widget=TextInput(attrs={'class': 'form-control input-sm chat-input', 'placeholder': 'Name'}))
+
+    email = forms.CharField(
+        widget=TextInput(attrs={'class': 'form-control input-sm chat-input', 'placeholder': 'Email'}))
+
+    text = forms.CharField(
+        widget=forms.Textarea(attrs={'class': 'form-control input-sm chat-input', 'placeholder': ''}))
+
     class Meta:
         model = Inquiry
         fields = ['name', 'email', 'text']
--- cms/apps/contact/views.py
+++ cms/apps/contact/views.py
@@ -3,12 +3,17 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.utils import timezone
+from django.contrib import messages
 
 from cms.apps.contact.forms import InquiryForm
 
 
 def index(request):
     form = InquiryForm()
+    if request.user.is_authenticated:
+        form.fields['name'].initial = request.user.username
+        form.fields['email'].initial = request.user.email
+
     return render(request, 'contact/index.html', {'form': form})
 
 
@@ -19,5 +24,6 @@ def save(request):
         model = form.save(commit=False)  # type: ModelForm
         model.created_at = timezone.now()
         model.save()
+        messages.add_message(request, messages.SUCCESS, 'Your message has been sent')
 
     return HttpResponseRedirect(reverse('contact:index'))
